24408f3dd9ddc0a9e1c58a305796ffd0f8a8593f
李磊 <1205710239@qq.com>
2020-09-14 14:04:17 +0800

    首次提交

---
+++ src/main/java/test/controller/testController.java
@@ -0,0 +1,21 @@
+package test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @ClassName:testController
+ * @Description:
+ * @Author lilei
+ * @Date 2020/9/14
+ * @Version 1.0
+ */
+@RestController
+public class testController {
+    @GetMapping(value = "/hi")
+    public String getHi(String name){
+        return "hi----"+name;
+    }
+}
---
+++ src/main/java/test/test.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName:test
+ * @Description:
+ * @Author lilei
+ * @Date 2020/9/11
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class test {
+    public static void main(String[] args) {
+        SpringApplication.run(test.class,args);
+    }
+}
