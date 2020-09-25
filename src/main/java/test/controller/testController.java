fd6df436e78790a697554f92909b53eaaf4e8fe9
李磊 <1205710239@qq.com>
2020-09-25 10:57:16 +0800

    首次提交

--- src/main/java/test/controller/testController.java
+++ src/main/java/test/controller/testController.java
@@ -3,6 +3,7 @@ package test.controller;
 
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import test.domain.test;
 
