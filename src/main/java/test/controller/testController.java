1543c46ab92ea07237d19ee139fa3aad3073785f
李磊 <1205710239@qq.com>
2020-09-29 11:11:35 +0800

    首次提交

--- src/main/java/test/controller/testController.java
+++ src/main/java/test/controller/testController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping(value = "/hi")
     public String getHi(){
-        return "hi----"+"devlop测试发布555555555";
+        return "hi----"+"devlop+6666666666";
     }
 
 }
