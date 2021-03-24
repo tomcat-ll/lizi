0f98f61d52831cf0af87127e8fab6a92eca336ae
lilei <1205710239@qq.com>
2021-03-24 15:48:47 +0800

    修改github凭证

--- src/main/java/test/service/serviceImp/picServiceImpl.java
+++ src/main/java/test/service/serviceImp/picServiceImpl.java
@@ -80,9 +80,9 @@ public class picServiceImpl implements picservice {
 
     public static String getUploadPath(String fileName,String upload){
 
-        //根据文件名称,生成hash字符串,截取前8位
+        //根据文件名称,生成hash字符串,截取前3位
         //1k2k2k3l5l6k3h4n4h4hn4nu4--1/k/2k/2k/3/l/5/l
-        String hash = Integer.toHexString(fileName.hashCode());
+        String hash = Integer.toHexString(3);
         while(hash.length()<3){
             hash += "0";
         }
