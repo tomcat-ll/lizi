c3bbea3baf60fe7c747ff70de8c17dfb8ed53f25
lilei <1205710239@qq.com>
2021-04-26 12:00:12 +0800

    k8s测试

--- src/main/java/test/controller/testController.java
+++ src/main/java/test/controller/testController.java
@@ -7,11 +7,8 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -38,7 +35,7 @@ public class testController {
         return "hi----"+"devlop+88888888";
     }
 
-    @ApiOperation(value = "图片上传")
+    @ApiOperation(value = "图片上传222222")
     @PostMapping(value = "/updateImage")
     public String updateImage(
        @ApiParam(value = "上传图片文件",required = true)@RequestParam() MultipartFile pic){
